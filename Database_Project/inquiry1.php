fa23c0c7105ea9acecfb26721a2553c1b1141eba
Karthikadyapady <karthik2fast4u@gmail.com>
2016-12-11 14:25:01 -0500

    Update inquiry1.php

--- Database_Project/inquiry1.php
+++ Database_Project/inquiry1.php
@@ -1,89 +1,71 @@
-1
 <!DOCTYPE html>
-2
 <html lang="en-us">
-3
 <meta charset="utf-8" />
 
 <head>
-6
     <title>Inquiry Form</title>
-7
     <link rel="stylesheet" href="css/Inquery.css">
-8
 </head>
-9
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-10
 </head>
-11
-​
-12
-<body>
-13
-    <div class="form">
-14
-        <div class="header">
-15
-            <h2>Inquiry Form</h2>
-16
-        </div>
-17
-        <div class="login">
-18
-            <form action="display.php" method="post">
-19
-                <ul>
-20
-                    <li>
-21
-                        <span class="un"><i class="fa fa-user"></i></span>
-22
-                        <input type="text" required class="text" placeholder="First Name" name="Firstname" />
-23
-                    </li>
-24
-                    <li>
-25
-                        <span class="un"><i class="fa fa-user"></i></span>
-26
-                        <input type="text" required class="text" placeholder="Last Name" name="Lastname" />
-27
-                    </li>
-28
-                    <li>
-29
-                        <span class="un"><i class="fa fa-user"></i></span>
-30
-                        <input type="text" required class="text" placeholder="Username" name="Username" />
-31
-                    </li>
-32
-                    <li>
-33
-                        <span class="un"><i class="fa fa-birthday-cake"></i></span>
-34
-                        <input type="number" pattern="0-9" required class="text" placeholder="Age" name="Age" />
-35
-                    </li>
-36
-                    <li>
-37
-                        <span class="un"><i class="fa fa-suitcase"></i></span>
-38
-                        <input type="text" required class="text" placeholder="Profession" name="Profession" />
-39
-                    </li>
-40
-                    <li>
-41
-                        Gender
-42
-                        <div class="span"><span class="ch"><input type="radio" name="Gender" value="male" required> Male<br>
-43
-                  <input type="radio" name="Gender" value="female"> Female<br></span>
-44
-                        </div>
-45
-                    </li>
 
+<body>
+    <div class="form">
+        <div class="header">
+            <h2>Inquiry Form</h2>
+        </div>
+        <div class="login">
+            <form action="display.php" method="post">
+                <ul>
+                    <li>
+                        <span class="un"><i class="fa fa-user"></i></span>
+                        <input type="text" required class="text" placeholder="First Name" name="Firstname" />
+                    </li>
+                    <li>
+                        <span class="un"><i class="fa fa-user"></i></span>
+                        <input type="text" required class="text" placeholder="Last Name" name="Lastname" />
+                    </li>
+                    <li>
+                        <span class="un"><i class="fa fa-user"></i></span>
+                        <input type="text" required class="text" placeholder="Username" name="Username" />
+                    </li>
+                    <li>
+                        <span class="un"><i class="fa fa-birthday-cake"></i></span>
+                        <input type="number" pattern="0-9" required class="text" placeholder="Age" name="Age" />
+                    </li>
+                    <li>
+                        <span class="un"><i class="fa fa-suitcase"></i></span>
+                        <input type="text" required class="text" placeholder="Profession" name="Profession" />
+                    </li>
+                    <li>
+                        Gender
+                        <div class="span"><span class="ch"><input type="radio" name="Gender" value="male" required> Male<br>
+                  <input type="radio" name="Gender" value="female"> Female<br></span>
+                        </div>
+                    </li>
+                    <li>
+                        Apartment type:
+                        <div class="span">
+                            <span class="ch">
+                     <select name="Apt_type">
+                        <option value="Studio" class="">Studio</option>
+                        <option value="1BHK">1BHK</option>
+                        <option value="2BHK">2BHK</option>
+                        <option value="3BHK">3BHK</option>
+                     </select>
+                     <br>
+                  </span>
+                        </div>
+                    </li>
+                    <li>
+                        <input type="submit" value="Submit" class="btn">
+                    </li>
+                </ul>
+            </form>
+
+        </div>
+        <br/>
+    </div>
+</body>
+
+</html>
